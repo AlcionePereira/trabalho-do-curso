320296131e5ce418e0e65b9c6f2dab5d9b08c457
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-09 18:09:15 -0300

    Add files via upload

---
+++ projdino.py
@@ -0,0 +1,35 @@
+print(" Parabéns!!!!!!!!!!")
+print(" Vai um dinossauro!")
+
+print("            ___")
+print("           /   )")
+print("          /   -")
+print("     .-^^^- /")
+print(" __/       /")
+print("<__.| |-| |")
+print("     -   -")
+print(" E aqui vai um bolo!" + " hummmmmm")
+print(" iiiiiiiiiiiiiiiiiii")
+print("#####################")
+print("=====================")
+print("#####################")
+print("=====================")
+print("#####################")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
