a6b33f7d4c346b098812feffd11f31efadd1508c
AlcionePereira <90060154+AlcionePereira@users.noreply.github.com>
2021-09-09 03:09:59 -0300

    Add files via upload

---
+++ gato.on.py
@@ -0,0 +1,82 @@
+print( " /\ __/\ ")
+print(" :Õ   Õ :")
+print(" : `^´  :" +"~~~~~~)")
+print("  \_~_/" + "´´´´´´´´)" +">>>>>>>")
+print("  (            )")
+print("   *-----------*")
+print("   II       II")
+print(" Meu animal preferido é o gato")
+print(" Moro em Teresina")
+print(" (^^^^^^^^^^^^^^)" +"        /\ ")
+print(" (  Ó   Ó   Ó  )" +"        /  \ ")
+print(" (      Ó   Ó  )" +"       /    \ ")
+print("  (  Ó         )" +"       ------ ")
+print("  *~~~~~~~~~~~~*" +"      |      | ")
+print("     )   (" +"            |      | ")
+print("     )   (" +"            |      | ")
+print("     )   (" +"            |      | ")        
+print("MMMMMMMMMMMMMMMMMMMMMM")
+
+
+
+
+
+
+      
+      
+
+
+
+
+
+      
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+
+
+
+
+
+
+      
+      
+      
+      
+      
+      
+      
+      
+
+
+
+
+
+
+
+
+
+
+
+      
+
+
+
+
+      
